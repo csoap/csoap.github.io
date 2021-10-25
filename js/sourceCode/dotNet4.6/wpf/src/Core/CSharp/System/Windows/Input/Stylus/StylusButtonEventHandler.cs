d5313fc73fd6c4aea3c833f3c940140f8250ef48
caizunzao <caizunzao@onemt.com.cn>
2021-10-25 14:22:43 +0800

    dotnet

--- js/sourceCode/dotNet4.6/wpf/src/Core/CSharp/System/Windows/Input/Stylus/StylusButtonEventHandler.cs
+++
@@ -1,8 +0,0 @@
-namespace System.Windows.Input
-{
-    /// <summary>
-    ///     The delegate to use for handlers that receive StylusButtonEventArgs.
-    /// </summary>
-    public delegate void StylusButtonEventHandler(object sender, StylusButtonEventArgs e);
-}
-
