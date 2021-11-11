044a07912403bd2a3b1d4b6d78b550b6878a5719
caizunzao <caizunzao@onemt.com.cn>
2021-11-11 20:24:20 +0800

    1

--- leetcode_csharp/.leetcode/100.相同的树.cs
+++ leetcode_csharp/.leetcode/100.相同的树.cs
@@ -19,8 +19,48 @@
  * }
  */
 public class Solution {
+    // 深度优先搜索
+    // public bool IsSameTree(TreeNode p, TreeNode q) {
+    //     if (p == null && q == null) return true;
+    //     if (p == null || q == null) return false;
+    //     if (p.val != q.val) return false;
+    //     return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+    // }
+    // 广度优先搜索
     public bool IsSameTree(TreeNode p, TreeNode q) {
-
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        Queue<TreeNode> queue1 = new Queue<TreeNode>();
+        Queue<TreeNode> queue2 = new Queue<TreeNode>();
+        queue1.Enqueue(p);
+        queue2.Enqueue(q);
+        while (queue1.Count != 0 && queue2.Count != 0)
+        {
+            TreeNode node1 = queue1.Dequeue();
+            TreeNode node2 = queue2.Dequeue();
+            if (node1.val != node2.val) return false;
+            TreeNode left1 = node1.left, right1 = node1.right, left2 = node2.left, right2 = node2.right;
+            if ((left1 == null) ^ (left2 == null)) return false; // 仅一个true 才为true
+            if ((right1 == null) ^ (right2 == null)) return false;
+            if (left1 != null)
+            {
+                queue1.Enqueue(left1);
+            }
+            if (right1 != null)
+            {
+                queue1.Enqueue(right1);
+            }
+            if (left2 != null)
+            {
+                queue2.Enqueue(left2);
+            }
+            if (right2 != null)
+            {
+                queue2.Enqueue(right2);
+            }
+        }
+        return queue1.Count == 0 && queue1.Count == 0;
     }
 }
 // @lc code=end
