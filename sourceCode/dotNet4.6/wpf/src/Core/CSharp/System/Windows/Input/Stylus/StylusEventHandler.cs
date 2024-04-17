2c8ea0c1c204b301a7d6e57434a5c540f0646e44
zunzao.cai <zunzao.cai@funplus.com>
2024-04-17 16:59:08 +0800

    remove sourceCode

--- sourceCode/dotNet4.6/wpf/src/Core/CSharp/System/Windows/Input/Stylus/StylusEventHandler.cs
+++
@@ -1,8 +0,0 @@
-namespace System.Windows.Input
-{
-    /// <summary>
-    ///     The delegate to use for handlers that receive StylusEventArgs.
-    /// </summary>
-    public delegate void StylusEventHandler(object sender, StylusEventArgs e);
-}
-
